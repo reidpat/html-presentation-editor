14ae118c57565a1f0d19fe980b4b1e175be4796c
reidpat <61036489+reidpat@users.noreply.github.com>
2024-03-30 16:15:06 -0700

    Langchain call working.

--- src/routes/langchain/+page.svelte
+++ src/routes/langchain/+page.svelte
@@ -1,29 +1,44 @@
 <script>
     import { writable } from "svelte/store";
-    import {onMount} from 'svelte'
+    import { onMount } from "svelte";
 
     let OpenAI_APIKey = writable("");
 
     // Save API key to local storage
-    function saveAPIKey(){
-        OpenAI_APIKey.subscribe(value => {
-            localStorage.setItem('OpenAI_APIKey', value);
+    function saveAPIKey() {
+        OpenAI_APIKey.subscribe((value) => {
+            localStorage.setItem("OpenAI_APIKey", value);
         })();
     }
 
     // Load API key from local storage
-    function loadAPIKey(){
-        const savedKey = localStorage.getItem('OpenAI_APIKey');
+    function loadAPIKey() {
+        const savedKey = localStorage.getItem("OpenAI_APIKey");
         if (savedKey) {
             OpenAI_APIKey.set(savedKey);
         }
     }
 
-    onMount(() => {
+    let chatModel;
+    let res = ""
+
+    onMount(async () => {
         loadAPIKey();
+
+        chatModel = new ChatOpenAI({
+            openAIApiKey: $OpenAI_APIKey,
+        });
+
+        res = await chatModel.invoke("what is LangSmith?");
+        console.log(res);
     });
+
+    import { ChatOpenAI } from "@langchain/openai";
 </script>
 
 <h1>Langchain Testing</h1>
-<input type="text" bind:value={$OpenAI_APIKey}>
+<input type="text" bind:value={$OpenAI_APIKey} />
 <button on:click={saveAPIKey}>Save API Key</button>
+<p>{res.content}</p>
+
+
